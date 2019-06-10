89d9336640f7bd0309de4c7d424268905eede3b9
Ikramullah Latif <sky_m4n@outlook.com>
2019-06-10 23:22:09 +0800

    [MODULE] Add warning for extender

--- src/commands/extend.js
+++ src/commands/extend.js
@@ -1,2 +1,10 @@
+/**
+ * Don't delete this file.
+ * 
+ * You can import this file from your command that using ES6.
+ * Not recommended if you writing in TypeScript,
+ * but it's work dude XD
+ */
+
 export * from 'discord.js';
 export { Client } from '../Bot';
