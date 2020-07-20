5d92b33dc5e1462894d797b117b11942081e7fc6
Ikramullah Latif <sky_m4n@outlook.com>
2020-07-20 10:06:42 +0800

    Initial settings

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,17 @@
-/**
- * Some predefined delays (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
+import { Client } from 'discord.js'
+import ConsoleStamp from 'console-stamp'
+import 'dotenv/config'
+
+ConsoleStamp(console)
+const client = new Client({
+  fetchAllMembers: false,
+  disableMentions: 'everyone'
+})
+
+if (process.env.PRODUCTION === 'DEV') {
+  client.on('debug', (message) => {
+    console.log(message)
+  })
 }
 
-/**
- * Returns a Promise<string> that resolves after given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - Number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
-}
-
-// Below are examples of using ESLint errors suppression
-// Here it is suppressing missing return type definitions for greeter function
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export async function greeter(name: string) {
-  return await delayedHello(name, Delays.Long);
-}
+client.login(process.env.TOKEN)
